8ca3d5edb503bf55ace217ec29ee779010b44c25
Tony Comito <ac106378@gmail.com>
2019-03-20 21:44:31 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
-let playeroneup = document.querySelector('div.player-one.up-arrow > img');
-let playeronedown = document.getElementsByClassName('player-one down-arrow');
-let playeroneleft = document.getElementsByClassName('player-one left-arrow');
-let playeroneright = document.getElementsByClassName('player-one right-arrow');
 
-let playertwoup = document.getElementsByClassName('player-two up-arrow');
-let playertwodown = document.getElementsByClassName('player-two down-arrow');
-let playertwoleft = document.getElementsByClassName('player-two left-arrow');
-let playertworight = document.getElementsByClassName('player-two right-arrow');
+let playeroneup = document.querySelector('.player-one.up-arrow > img');
+let playeronedown = document.querySelector('.player-one.down-arrow > img');
+let playeroneleft = document.querySelector('.player-one.left-arrow > img');
+let playeroneright = document.querySelector('.player-one.right-arrow > img');
+
+
+let playertwoup = document.querySelector('.player-two.up-arrow > img');
+let playertwodown = document.querySelector('.player-two.down-arrow > img');
+let playertwoleft = document.querySelector('.player-two.left-arrow > img');
+let playertworight = document.querySelector('.player-two.right-arrow > img');
 
 let backgroundtile = 'images/background.png';
 let playerone = 'images/robot.png';
 let playertwo = 'images/zombie.png';
+
+let playeronerow = 1;
+let playeronecol = 1;
+let playertworow = 1;
+let playertwocol = 1;
+
 
 playeroneup.onclick = () => {
     let currenttile = document.getElementsByClassName('robot');
@@ -18,3 +26,44 @@ playeroneup.onclick = () => {
     console.log(classname);
 };
 
+playeroneleft.onclick = () => {
+    let currenttile = document.getElementsByClassName('robot');
+    let classname = document.querySelector('div < img[src="images/robot.png"]:parent').className;
+    console.log(classname);
+};
+
+playeroneright.onclick = () => {
+    let currenttile = document.getElementsByClassName('robot');
+    let classname = document.querySelector('div < img[src="images/robot.png"]:parent').className;
+    console.log(classname);
+};
+
+playeronedown.onclick = () => {
+    let currenttile = document.getElementsByClassName('robot');
+    let classname = document.querySelector('div < img[src="images/robot.png"]:parent').className;
+    console.log(classname);
+};
+
+playertwoup.onclick = () => {
+    let currenttile = document.getElementsByClassName('zombie');
+    let classname = document.querySelector('div < img[src="images/zombie.png"]:parent').className;
+    console.log(classname);
+};
+
+playertwoleft.onclick = () => {
+    let currenttile = document.getElementsByClassName('zombie');
+    let classname = document.querySelector('div < img[src="images/zombie.png"]:parent').className;
+    console.log(classname);
+};
+
+playertworight.onclick = () => {
+    let currenttile = document.getElementsByClassName('zombie');
+    let classname = document.querySelector('div < img[src="images/zombie.png"]:parent').className;
+    console.log(classname);
+};
+
+playertwodown.onclick = () => {
+    let currenttile = document.getElementsByClassName('zombie');
+    let classname = document.querySelector('div < img[src="images/zombie.png"]:parent').className;
+    console.log(classname);
+};

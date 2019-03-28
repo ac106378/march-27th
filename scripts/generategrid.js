39a55c6fef43cd23c8cb898a92b52a20316bbec4
Tony Comito <ac106378@gmail.com>
2019-03-27 21:26:05 -0700

    finished script2.js

--- scripts/generategrid.js
+++ scripts/generategrid.js
@@ -2,20 +2,19 @@
 var container = document.querySelector('.rc-container');
 
 for (var row = 1; row <= 8; row++) {
-    for (var col = 1; col <= 8; col++) {
-        console.log("TEST");
-        //Create Child
-        var div = document.createElement('div');
-        var className = `rc r-${row} c-${col}`;
-        div.setAttribute('class', className);
+  for (var col = 1; col <= 8; col++) {
+    // Create Child
+    var div = document.createElement('div');
+    var className = `rc r-${row} c-${col}`;
+    div.setAttribute('class', className);
 
-        var img = document.createElement('img');
-        img.setAttribute('src', 'images/background.png');
-        img.setAttribute('alt', 'background tile image');
-        div.appendChild(img);
-        //Append Child
-        container.appendChild(div);
-    }
+    var img = document.createElement('img');
+    img.setAttribute('src', 'images/background.png');
+    img.setAttribute('alt', 'background tile image');
+    div.appendChild(img);
+    // Append Child
+    container.appendChild(div);
+  }
 }
 document.querySelector('.rc.r-1.c-1').classList.add('robot');
 document.querySelector('.rc.r-8.c-8').classList.add('zombie');
